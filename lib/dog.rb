c9bd82a8dcec7e138bd2c56f5dcd9df2aa1315b8
Will Andre <wandre94@gmail.com>
2021-01-21 17:26:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
-# Add your code here
+class Dog
+
+  @@all = []
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def clear_all
+  end
+
+  def print_all
+  end
+
+
+end
